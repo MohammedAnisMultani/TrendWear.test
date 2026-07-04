105dc9d8bbdd270164c0ed58cac304753e4846f7
MohammedAnis <mohammedanismultani@gmail.com>
2026-07-04 14:38:57 +0530

    cors update

--- server.js
+++ server.js
@@ -29,21 +29,10 @@
 
     // ---------
 
-    const allowedOrigins = [
-    "http://localhost:5173",
-    "https://trend-wear-test-z9so.vercel.app"
-    ];
-
-    app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-        } else {
-        callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true
-    }));
+  app.use(cors({
+  origin: ["http://localhost:5173", "https://trend-wear-test-z9so.vercel.app"],
+  credentials: true
+}));
 
     // ----------
 
